cfe28e010d048070c3414216891265ce29727389
Kentaro-Asada <asaponkenpon@gmail.com>
2021-04-17 02:34:08 +0900

    rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_162924) do
+ActiveRecord::Schema.define(version: 2021_04_16_171252) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -246,6 +246,7 @@ ActiveRecord::Schema.define(version: 2021_04_10_162924) do
     t.string "name", null: false
     t.string "profile_photo"
     t.text "info"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
